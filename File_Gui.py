9711b03e5722db038bd1bd4700ef2c8a6be9bf79
Amhepel <36275584+Amhepel@users.noreply.github.com>
2019-11-11 13:51:05 -0500

    Add files via upload
    Mouse clicks. Need to fix how mouse clicks are read

--- File_Gui.py
+++ File_Gui.py
@@ -8,13 +8,12 @@ from tkinter import *
 from tkinter import filedialog
 import imageio
 from PIL import Image, ImageTk
+from pynput.mouse import Controller
 
+import os
 
 
 class Root(Tk):
-
-
-
 
     def __init__(self):
         # Constructor, Makes version 
@@ -34,51 +33,92 @@ class Root(Tk):
         self.labelFrame = ttk.LabelFrame(self, text = "Open A Video File")
         self.labelFrame.grid(row = 1, column = 0, padx = 10, pady = 20)
         self.button()
-        
         # self.printf()
+        self.mouse = Controller()
+        global coord
+        coord = ''
+        self.pt_1 = ttk.Label(self, text = coord)
+        self.pt_1.grid(row = 4, column = 6)
+
+        """Create Submit Button"""
+        # self.photo = Image.open("C:\Users\mhepel\Pictures\Cars_1.jpeg")
+        self.submitButton = Button(self, command=self.buttonClick, text="Submit")
+        self.submitButton.grid(row = 4, column = 5)
+        
+        # self.click()
+
+    def buttonClick(self):
+        """ handle button click event and output text from entry area"""
+        print('hello')    # do here whatever you want
+        global coord
+        coord = "f {0}".format(self.mouse.position)
+        self.pt_1.configure(text = coord)
+        print(coord)
+        # self.click()
+    
+
+
+
+    global vid_x 
+    vid_x = 500 # Vid Width
+    global vid_y 
+    vid_y = 300 # Vid Height
+
+    # def click(self):
+      #  global vid_x
+       # global vid_y
+       # x = vid_x + 100
+       # y = vid_y + 100
+       # self.vid_button = ttk.Button(self)
+       # self.vid_button.config(width = x, height = y)
+       # self.vid_button.grid(row = 0, column = 0)
+       # print("x: {0}".format(self.mouse.position))
 
     def button(self):
         # Create Button to open file directory
-        
         self.button = ttk.Button(self.labelFrame, text = "File Browser", command = self.fileDialog)
-        self.button.grid(row = 2, column = 0)
+        self.button.grid(row = 1, column = 1)
+
+    
+    
+
+    
+    
 
 
     def fileDialog(self):
         #Reads file to video and displays video in Gui
+        global vid_x
         vid_x = 500 # Vid Width
+        global vid_y
         vid_y = 300 # Vid Height
-
-
-        self.filename = filedialog.askopenfilename(initialdir = '/', title = "Select a File") # Read File
+        self.filename = filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select a File") # Read File
         self.label = ttk.Label(self)
-        self.label.grid(row = 3, column = 0)
+        self.label.grid(row = 2, column = 0)
         self.label.configure(text = self.filename) # Display filename
         cap = cv2.VideoCapture(self.filename) # Play video of file
-        while True:
-            # Video 
-            _, self.frame = cap.read()
-            # cv2.imshow('frame', self.frame)
-            # self.frame = cv2.flip(self.frame, 1)
-            cv2image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)
-            img = Image.fromarray(cv2image)
 
-            copy_of_image = img.copy() 
-            img = copy_of_image.resize((vid_x,vid_y)) # size of video
-            imgtk = ImageTk.PhotoImage(image=img)
-    
-            self.lmain.imgtk = imgtk
-            self.lmain.configure(image=imgtk)
-            self.lmain.after(10, filedialog) 
+        # while True:
+        # Video 
+        _, self.frame = cap.read()
+        # cv2.imshow('frame', self.frame)
+        frame = cv2.flip(self.frame, 1)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
 
+        copy_of_image = img.copy() 
+        img = copy_of_image.resize((vid_x,vid_y)) # size of video
+        imgtk = ImageTk.PhotoImage(image=img)
 
+        self.lmain.imgtk = imgtk
+        self.lmain.configure(image=imgtk)
 
+        self.newbut = ttk.Button(self.labelFrame, image = img, command = self.buttonClick)
+        self.newbut.grid(row = 0, column = 0)
+        # self.lmain.after(10, self.fileDialog) 
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-            input('q')
-            self.quit()
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
         cap.release()
         cv2.destroyAllWindows()
         # return self.filename
@@ -89,12 +129,3 @@ class Root(Tk):
 if __name__ == '__main__':
     root = Root()
     root.mainloop()
-
-
-
-
-
-
-
-
-
