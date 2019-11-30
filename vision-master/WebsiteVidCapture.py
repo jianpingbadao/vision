25a7c8081bf7e2be675d150669d21a081b9e8118
paulp123 <47534946+paulp123@users.noreply.github.com>
2019-11-30 14:02:17 -0500

    Add files via upload

--- vision-master/WebsiteVidCapture.py
+++ vision-master/WebsiteVidCapture.py
@@ -9,11 +9,13 @@ def run(website_name):
 	driver = webdriver.Chrome(executable_path='/Users/paulyp123/Desktop/chromedriver')
 	#https://nyctmc.org/google_popup.php?cid=975
 	driver.get(website_name)
-	#driver.get('https://www.youtube.com/')
-	image_folder = '/Users/paulyp123/Desktop/vision-master/' + str(time.time())
+	strTime = str(time.time())
+	image_folder = '/Users/paulyp123/Desktop/vision-master/' + strTime
 	os.mkdir(image_folder)
 	os.chdir(image_folder)
 
+	#take multiple screenshots
+	#figure out how many duplicates you need
 	for x in range(20):
 		time.sleep(.1)
 		driver.save_screenshot(str(time.time()) + ".png")
@@ -33,15 +35,19 @@ def run(website_name):
 	width = right - left
 	height = down - top
 	video = cv2.VideoWriter(video_name, 0, 1, (width, height))
+	#
 
 	for image in images:
 		im = Image.open(image)
 		crop = im.crop((left, top, right, down))
 		crop.save(image)
-
-	video.write(cv2.imread(os.path.join(image_folder, image)))
+		video.write(cv2.imread(os.path.join(image_folder, image)))
+	#os.chdir('/Users/paulyp123/Desktop/vision-master/')
+	
 
 	cv2.destroyAllWindows()
 	video.release()
-	for image in images:
-		os.remove(image)
+	#for image in images:
+		#os.remove(image)
+
+	return strTime
