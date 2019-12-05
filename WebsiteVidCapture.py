e54be5f57367e6b30244cfe476c54c512d8de412
weida zhong <weida.ustc@gmail.com>
2019-12-04 20:34:55 -0500

    automatically get the crop border of the camera view from the screenshot

--- WebsiteVidCapture.py
+++ WebsiteVidCapture.py
@@ -7,6 +7,7 @@ import cv2
 import os
 import sys
 from datetime import datetime
+import numpy as np
 
 
 desktop_path = os.path.expanduser("~/Desktop/")
@@ -26,6 +27,41 @@ def get_current_time():
     return now.strftime("%Y-%m-%d-%H-%M-%S-%f")
 
 
+def get_image_border(image_path):
+    """
+    Get the border of the camera from the image so as to crop the image.
+    
+    Parameters
+    ----------
+    image_path : String
+        The absolute path of the image to be dealt with
+    
+    Returns
+    -------
+    left, top, right, down
+        Two coordinates of the points, i.e., top left corner and bottom right cornor.
+    """
+    img = Image.open(image_path)
+    img_array = np.asarray(img)
+
+    height, width, _ = img_array.shape
+
+    left = width  # the minimum x with non-white
+    top = height  # the minimum y with non-white
+    right = 0  # the largest x with non-white
+    down = 0  # the largest y with non-white
+
+    for row in range(height):
+        for col in range(width):
+            if img_array[row][col][0] != 255:
+                top = min(row, top)
+                left = min(col, left)
+                down = max(row, down)
+                right = max(col, right)
+
+    return left, top, right, down
+
+
 def run(website_name):
     global desktop_path
     driver = webdriver.Chrome(executable_path=chromedriver_path)
@@ -39,7 +75,8 @@ def run(website_name):
 
     # take multiple screenshots
     # figure out how many duplicates you need
-    for x in range(20):
+    time.sleep(1)
+    for x in range(10):
         time.sleep(1)
         driver.save_screenshot(str(time.time()) + ".png")
 
@@ -50,13 +87,20 @@ def run(website_name):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
     images.sort()
 
-    left = 848
-    top = 127
-    right = 1552
-    down = 541
+    # get the border from the image
+    if not images:
+        print(f"Error: NO screenshot has been captured from {website_name}")
+        sys.exit(0)
+
+    left, top, right, down = get_image_border(images[0])
+    if left >= right or top >= down:
+        print(f"Error: Incorrect border of the camera has been detected from image: {images[0]} of URL {website_name}")
+        print(f"left: {left}, top: {top}, right: {right}, down: {down}")
+        sys.exit(0)
+
     width = right - left
     height = down - top
-    video = cv2.VideoWriter(video_name, 0, 1, (width, height))
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'XVID'), 1, (width, height))
 
     for image in images:
         im = Image.open(image)
@@ -66,8 +110,8 @@ def run(website_name):
 
     cv2.destroyAllWindows()
     video.release()
-    for image in images:
-        os.remove(image)
+    # for image in images:
+    #     os.remove(image)
 
     return strTime
 
