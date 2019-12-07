4254fe50feb30f4e144590a88614b1cbcd06c7b8
weida zhong <weida.ustc@gmail.com>
2019-12-06 21:06:45 -0500

    reformat the vehicles.py

--- vehicles.py
+++ vehicles.py
@@ -1,28 +1,31 @@
 from random import randint
 import time
 
-class Car:
-    tracks=[]
-    def __init__(self,i,xi,yi,max_age):
-        self.i=i
-        self.x=xi
-        self.y=yi
-        self.tracks=[]
-        self.R=randint(0,255)
-        self.G=randint(0,255)
-        self.B=randint(0,255)
-        self.done=False
-        self.state='0'
-        self.age=0
-        self.max_age=max_age
-        self.dir=None
 
-    def getRGB(self):  #For the RGB colour
-        return (self.R,self.G,self.B)
+class Car:
+    tracks = []
+
+    def __init__(self, i, xi, yi, max_age):
+        self.i = i
+        self.x = xi
+        self.y = yi
+        self.tracks = []
+        self.R = randint(0, 255)
+        self.G = randint(0, 255)
+        self.B = randint(0, 255)
+        self.done = False
+        self.state = '0'
+        self.age = 0
+        self.max_age = max_age
+        self.dir = None
+
+    def getRGB(self):  # For the RGB colour
+        return (self.R, self.G, self.B)
+
     def getTracks(self):
         return self.tracks
 
-    def getId(self): #For the ID
+    def getId(self):  # For the ID
         return self.i
 
     def getState(self):
@@ -31,10 +34,10 @@ class Car:
     def getDir(self):
         return self.dir
 
-    def getX(self):  #for x coordinate
+    def getX(self):  # for x coordinate
         return self.x
 
-    def getY(self):  #for y coordinate
+    def getY(self):  # for y coordinate
         return self.y
 
     def updateCoords(self, xn, yn):
@@ -50,11 +53,11 @@ class Car:
         return self.done
 
     def going_UP(self, mid_start, mid_end):
-        if len(self.tracks)>=2:
-            if self.state=='0':
-                if self.tracks[-1][1]<mid_end and self.tracks[-2][1]>=mid_end:
-                    state='1'
-                    self.dir='up'
+        if len(self.tracks) >= 2:
+            if self.state == '0':
+                if self.tracks[-1][1] < mid_end and self.tracks[-2][1] >= mid_end:
+                    state = '1'
+                    self.dir = 'up'
                     return True
                 else:
                     return False
@@ -63,12 +66,12 @@ class Car:
         else:
             return False
 
-    def going_DOWN(self,mid_start,mid_end):
-        if len(self.tracks)>=2:
-            if self.state=='0':
-                if self.tracks[-1][1]>mid_start and self.tracks[-2][1]<=mid_start:
-                    start='1'
-                    self.dir='down'
+    def going_DOWN(self, mid_start, mid_end):
+        if len(self.tracks) >= 2:
+            if self.state == '0':
+                if self.tracks[-1][1] > mid_start and self.tracks[-2][1] <= mid_start:
+                    start = '1'
+                    self.dir = 'down'
                     return True
                 else:
                     return False
@@ -78,20 +81,21 @@ class Car:
             return False
 
     def age_one(self):
-        self.age+=1
-        if self.age>self.max_age:
-            self.done=True
-        return  True
+        self.age += 1
+        if self.age > self.max_age:
+            self.done = True
+        return True
 
-#Class2
+
+# Class2
 
 class MultiCar:
-    def __init__(self,cars,xi,yi):
-        self.cars=cars
-        self.x=xi
-        self.y=yi
-        self.tracks=[]
-        self.R=randint(0,255)
-        self.G=randint(0,255)
-        self.B=randint(0,255)
-        self.done=False
+    def __init__(self, cars, xi, yi):
+        self.cars = cars
+        self.x = xi
+        self.y = yi
+        self.tracks = []
+        self.R = randint(0, 255)
+        self.G = randint(0, 255)
+        self.B = randint(0, 255)
+        self.done = False
