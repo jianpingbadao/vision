2737a9dd422ef57273be2f62c2cbb2f37eff9e49
weida zhong <weida.ustc@gmail.com>
2019-12-13 16:17:52 -0500

    retrieve info from the parameters

--- video_process.py
+++ video_process.py
@@ -5,9 +5,11 @@ import time
 import os
 from typing import List
 
+from utils import Hexagon, Point
+
 debug = True
 
-def execute(video_file: str, result_file: str, lines: List) -> None:
+def video_process(video_file: str, result_file: str, lines: List) -> None:
     """
     Process video and count the number of vehicles passing by
 
@@ -48,7 +50,24 @@ def execute(video_file: str, result_file: str, lines: List) -> None:
     width = cap.get(3)
     height = cap.get(4)
     frame_area = height * width
-    area_threshold = frame_area / 400  # the area threshold (minimum) for each single vehicle in the video;
+    area_threshold = frame_area / 450  # the area threshold (minimum) for each single vehicle in the video;
     # TODO: should be adjustable for different camera
     # one option is to get the width of the lane
     # then use that as the baseline
+
+    # construct Hexagon area using the passed/picked up lines
+    hexagons = []
+    for direction_and_line in lines:
+        direction = direction_and_line['direction']
+        one_group_lines = direction_and_line['lines']
+
+        # change the number in lines from ratio to actual values
+        # based on the width and height of the window
+        new_group_lines = []
+        for line in one_group_lines:
+            new_line = [int(num * width) if idx % 2 == 0 else int(num * height) for idx, num in enumerate(line)]
+            new_group_lines.append(new_line)
+        hexagon = Hexagon(new_group_lines, direction)
+        hexagons.append(hexagon)
+    all_counts = [0] * len(lines)
+    all_cars = [[] for _ in range(len(lines))]  # the Car in each hexagon
