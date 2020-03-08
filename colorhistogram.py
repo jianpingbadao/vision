599cefd13567d5b302b203afd6905c5bb450241b
paulp123 <47534946+paulp123@users.noreply.github.com>
2020-03-08 19:22:16 -0400

    color histogram extraction
    In this commit, I extracted a color histogram from an image.

---
+++ colorhistogram.py
@@ -0,0 +1,19 @@
+#make sure to import matplotlib
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+#can also use "Car1.png" or "Car3.png"
+img = cv2.imread("Car2.png")
+b, g, r = cv2.split(img)
+
+cv2.imshow("img", img)
+cv2.imshow("b", b)
+cv2.imshow("g", g)
+cv2.imshow("r", r)
+
+plt.hist(b.ravel(), 256, [0, 256])
+plt.hist(g.ravel(), 256, [0, 256])
+plt.hist(r.ravel(), 256, [0, 256])
+plt.show()
